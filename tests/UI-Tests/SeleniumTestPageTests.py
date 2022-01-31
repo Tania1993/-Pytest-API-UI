a5e84bf38b3377d206be6829998abe445377ca19
Tania Haiuk <t.v.gayuk@gmail.com>
2022-01-31 18:45:07 +0200

    refactiring, UI mark added, pytest.ini

--- tests/UI-Tests/SeleniumTestPageTests.py
+++ tests/UI-Tests/SeleniumTestPageTests.py
@@ -1,15 +1,18 @@
 import configparser
+import pytest
 
 
 class TestSeleniumMainPage:
-    # вызываем фикстуру в тесте, передав ее как параметр
     config = configparser.ConfigParser()
-    link = config["URLs"]["selenium_test_page"]
+    config.read('config.ini')
+    link = config['URLs']['selenium_test_page']
 
+    @pytest.mark.UI
     def test_guest_should_see_login_link(self, browser):
         browser.get(self.link)
-        browser.find_element_by_css_selector("#login_link")
+        browser.find_element_by_css_selector('#login_link')
 
+    @pytest.mark.UI
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(self.link)
-        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+        browser.find_element_by_css_selector('.basket-mini .btn-group > a')
