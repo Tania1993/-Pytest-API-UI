0398f25ac7d9bd8f0e1fa1900f7ec77c141402bb
Tania Haiuk <t.v.gayuk@gmail.com>
2022-02-03 15:18:40 +0200

    lang keyword param added

--- tests/UI-Tests/SeleniumTestPageTests.py
+++ tests/UI-Tests/SeleniumTestPageTests.py
@@ -1,20 +1,25 @@
-import configparser
 import pytest
-import os
+import configparser
 
 
+#@pytest.mark.parametrize('language', ['en-gb', 'ru'])
 class TestSeleniumMainPage:
     config = configparser.ConfigParser()
-    #path = '/'.join((os.path.abspath(__file__).replace('\\', '/')).split('/')[:-1])
     config.read('config.ini')
     link = config['URLs']['selenium_test_page']
 
     @pytest.mark.UI
     def test_guest_should_see_login_link(self, browser):
-        browser.get(self.link)
+        browser.get(f'{self.link}/')
         browser.find_element_by_css_selector('#login_link')
 
     @pytest.mark.UI
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
-        browser.get(self.link)
+        browser.get(f'{self.link}/')
         browser.find_element_by_css_selector('.basket-mini .btn-group > a')
+
+    @pytest.mark.xfail(reason='fixing')
+    @pytest.mark.UI
+    def test_guest_should_see_search_button_on_the_main_page(self, browser):
+        browser.get(f'{self.link}/')
+        browser.find_element_by_css_selector('button.favorite')
--- tests/UI-Tests/conftest.py
+++ tests/UI-Tests/conftest.py
@@ -1,13 +1,31 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help='Choose browser: chrome or firefox')
+    parser.addoption('--language', default='en-gb',
+                     help='Enter a language: en-gb, es, fr, ua, ru')
 
 
 @pytest.fixture(scope='class')
-def browser():
-    print('\nstart browser for test..')
-    browser = webdriver.Chrome(ChromeDriverManager().install())
+def browser(request):
+    browser_name = request.config.getoption('browser_name')
+    user_language = request.config.getoption('language')
+    if browser_name == 'chrome':
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    elif browser_name == 'firefox':
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference('intl.accept_languages', user_language)
+        browser = webdriver.Firefox(GeckoDriverManager().install(), firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
 
-    print('\nquit browser..')
     browser.quit()
