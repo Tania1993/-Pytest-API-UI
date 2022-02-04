20f46841f4ca0cd52e78d74a28a0646e2b42d071
Tania Haiuk <t.v.gayuk@gmail.com>
2022-02-04 12:18:56 +0200

    firefox driver added

--- tests/UI-Tests/conftest.py
+++ tests/UI-Tests/conftest.py
@@ -16,14 +16,14 @@ def pytest_addoption(parser):
 def browser(request):
     browser_name = request.config.getoption('browser_name')
     user_language = request.config.getoption('language')
+    options = Options()
     if browser_name == 'chrome':
-        options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        browser = webdriver.Chrome(ChromeDriverManager().install())
+        browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     elif browser_name == 'firefox':
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference('intl.accept_languages', user_language)
-        browser = webdriver.Firefox(GeckoDriverManager().install(), firefox_profile=fp)
+        # fp = webdriver.FirefoxProfile()
+        # fp.set_preference('intl.accept_languages', user_language)
+        browser = webdriver.Firefox()
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
